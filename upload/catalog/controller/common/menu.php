b51f7aefd6a9d222e69b7e590e60e52a44ef31ee
captainerd <frapenation@gmail.com>
2024-01-24 13:13:31 +0200

    Menu code enhancement

--- upload/catalog/controller/common/menu.php
+++ upload/catalog/controller/common/menu.php
@@ -11,45 +11,35 @@ class Menu extends \Opencart\System\Engine\Controller {
 	 */
 	public function index(): string {
 		$this->load->language('common/menu');
-
-		// Menu
 		$this->load->model('catalog/category');
-
 		$this->load->model('catalog/product');
+	
+		$data['categories'] = $this->buildMenu(0);
+	 
+		return $this->load->view('common/menu', $data);
 
-		$data['categories'] = [];
-
-		$categories = $this->model_catalog_category->getCategories(0);
-
+	}
+	
+	protected function buildMenu($parent_id) {
+		$categories = $this->model_catalog_category->getCategories($parent_id);
+		$menu = [];
+	
 		foreach ($categories as $category) {
-			if ($category['top']) {
-				// Level 2
-				$children_data = [];
+	 
 
-				$children = $this->model_catalog_category->getCategories($category['category_id']);
-
-				foreach ($children as $child) {
-					$filter_data = [
-						'filter_category_id'  => $child['category_id'],
-						'filter_sub_category' => true
-					];
-
-					$children_data[] = [
-						'name'  => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
-						'href'  => strlen($child['redirect_url']) > 0  ? $child['redirect_url'] :  $this->url->link('product/category', 'language=' . $this->config->get('config_language') . '&path=' . $category['category_id'] . '_' . $child['category_id'])
-					];
-				}
-
-				// Level 1
-				$data['categories'][] = [
+				$children_data = $this->buildMenu($category['category_id']);
+			 
+	
+				$menu[] = [
 					'name'     => $category['name'],
+					'image'     => $category['image'],
 					'children' => $children_data,
 					'column'   => $category['column'] ? $category['column'] : 1,
 					'href'     => strlen($category['redirect_url']) > 0 ? $category['redirect_url'] : $this->url->link('product/category', 'language=' . $this->config->get('config_language') . '&path=' . $category['category_id'])
 				];
-			}  
+			 
 		}
-		 
-		return $this->load->view('common/menu', $data);
+	
+		return $menu;
 	}
 }
