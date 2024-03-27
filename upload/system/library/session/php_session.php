f0d61a92c62c3936655a77d8f41d1ccedd7670a5
captainerd <frapenation@gmail.com>
2024-03-27 12:03:09 +0200

    System(Fix Session class php_session)

--- upload/system/library/session/php_session.php
+++ upload/system/library/session/php_session.php
@@ -27,12 +27,7 @@ class PhpSession {
 	 
 		$this->config = $registry->get('config');
  
-        session_set_cookie_params(
-            time() + $this->config->get('session_expire'), // Lifetime in seconds
-            $this->config->get('session_path'),   // Path
-            $this->config->get('session_domain'), // Domain
-            $this->config->get('session_samesite'),// SameSite
-        );
+        session_set_cookie_params( time() + $this->config->get('session_expire'), '/', $this->config->get('session_domain'));
         session_start();
 
 	}
