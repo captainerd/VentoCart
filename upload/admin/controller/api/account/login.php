2ee88a991e72b885c8338b118001bd1ca5ecef2c
natsos <natsos@fedora.lan>
2024-12-19 20:15:06 +0200

    api fix

--- upload/admin/controller/api/account/login.php
+++ upload/admin/controller/api/account/login.php
@@ -5,11 +5,13 @@ namespace Ventocart\Admin\Controller\Api\Account;
  *
  * @package Ventocart\Admin\Controller\Api\Account
  */
-class Login extends \Ventocart\System\Engine\Controller {
- 
-	public function index(): void {
+class Login extends \Ventocart\System\Engine\Controller
+{
+
+	public function index(): void
+	{
 		$this->load->language('api/account/login');
- 
+
 		$json = [];
 
 		$this->load->model('api/account');
@@ -20,28 +22,16 @@ class Login extends \Ventocart\System\Engine\Controller {
 		if (!isset($this->request->post['key']) && isset($this->request->headers['KEY'])) {
 			$this->request->post['key'] = trim($this->request->headers['KEY']);
 		}
-	 
+
 		// Login with API Key
 		if (!empty($this->request->post['username']) && !empty($this->request->post['key'])) {
+
 			$api_info = $this->model_api_account->login($this->request->post['username'], $this->request->post['key']);
+
 		} else {
 			$api_info = [];
 		}
 
-		if ($api_info) {
-			// Check if IP is allowed
-			$ip_data = [];
-
-			$results = $this->model_api_account->getIps($api_info['api_id']);
-
-			foreach ($results as $result) {
-				$ip_data[] = trim($result['ip']);
-			}
-
-	 
-		} else {
-			$json['error'] = $this->language->get('error_key');
-		}
 
 		if (!$json) {
 			$json['success'] = $this->language->get('text_success');
