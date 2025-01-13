6127e71d89b56c571321f76672e948b39e5fe3b9
natsos <natsos@fedora.lan>
2025-01-13 19:55:55 +0200

    fix(security): autologout on nosession token

--- upload/admin/controller/error/permission.php
+++ upload/admin/controller/error/permission.php
@@ -5,26 +5,21 @@ namespace Ventocart\Admin\Controller\Error;
  *
  * @package Ventocart\Admin\Controller\Error
  */
-class Permission extends \Ventocart\System\Engine\Controller {
+class Permission extends \Ventocart\System\Engine\Controller
+{
 	/**
 	 * @return void
 	 */
-	public function index(): void {
+	public function index(): void
+	{
 		$this->load->language('error/permission');
 
 		$this->document->setTitle($this->language->get('heading_title'));
 
-		$data['breadcrumbs'] = [];
-
-		$data['breadcrumbs'][] = [
-			'text' => $this->language->get('text_home'),
-			'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'])
-		];
-
-		$data['breadcrumbs'][] = [
-			'text' => $this->language->get('heading_title'),
-			'href' => $this->url->link($this->request->get['route'], 'user_token=' . $this->session->data['user_token'])
-		];
+		if (!isset($this->session->data['user_token'])) {
+			$this->request->get['route'] = 'common/logout';
+			$this->load->controller('common/logout');
+		}
 
 		$data['header'] = $this->load->controller('common/header');
 		$data['column_left'] = $this->load->controller('common/column_left');
