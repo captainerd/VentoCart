bf1d60eae25f333cc44870c84738ab1a1a3efebb
natsos <natsos@fedora.lan>
2025-01-14 14:42:44 +0200

    feat(installer): Extension install backward compatibility to OC4, OC3, OC2

--- upload/admin/controller/marketplace/installer.php
+++ upload/admin/controller/marketplace/installer.php
@@ -54,8 +54,8 @@ class Installer extends \Ventocart\System\Engine\Controller
 
 
 	/* 
-																																															Extracts an theme out into a zip file
-																																															*/
+																																																																																																																																																																																																																																																																																																  Extracts an theme out into a zip file
+																																																																																																																																																																																																																																																																																																  */
 
 	public function downloadTheme(): void
 	{
@@ -108,8 +108,8 @@ class Installer extends \Ventocart\System\Engine\Controller
 		}
 	}
 	/* 
-																																																		Extracts an extension out into a zip file
-																																																  */
+																																																																																																																																																																																																																																																																																																			  Extracts an extension out into a zip file
+																																																																																																																																																																																																																																																																																																		*/
 	public function download(): void
 	{
 		$code = $this->request->get['code'];
@@ -379,7 +379,7 @@ class Installer extends \Ventocart\System\Engine\Controller
 		// 1. Validate the file uploaded.
 		if (isset($this->request->files['file']['name'])) {
 			$filename = basename($this->request->files['file']['name']);
-
+			$filename = str_replace('.ocmod', '.vemod', $filename);
 			// Check the file extension
 			if (substr($filename, -10) == '.vemod.zip') {
 				$code = basename($filename, '.vemod.zip');
@@ -390,7 +390,7 @@ class Installer extends \Ventocart\System\Engine\Controller
 
 				$json['error'] = $this->language->get('error_file_type');
 			}
-
+			$code = strtolower($code);
 			// 2. Validate the filename length.
 			if ((oc_strlen($filename) < 1) || (oc_strlen($filename) > 128)) {
 				$json['error'] = $this->language->get('error_filename');
@@ -423,27 +423,34 @@ class Installer extends \Ventocart\System\Engine\Controller
 			if (!$theme) {
 				if ($zip->open($file, \ZipArchive::RDONLY)) {
 					$install_info = json_decode($zip->getFromName('install.json'), true);
+					$keys = [
+						'extension_id',
+						'extension_download_id',
+						'name',
+						'description',
+						'code',
+						'version',
+						'author',
+						'link'
+					];
+					if (!$install_info) {
 
-					if ($install_info) {
-						$keys = [
-							'extension_id',
-							'extension_download_id',
-							'name',
-							'description',
-							'code',
-							'version',
-							'author',
-							'link'
+
+						$install_info = [
+							'name' => ucfirst($code),
+							'description' => $code,
+							'code' => $code,
+							'version' => "1.0",
+							'author' => 'VentoCart',
+							'link' => 'http://ventocart.com',
 						];
-
-						foreach ($keys as $key) {
-							if (!isset($install_info[$key])) {
-								$install_info[$key] = '';
-							}
-						}
-					} else {
-						$json['error'] = $this->language->get('error_install');
 					}
+					foreach ($keys as $key) {
+						if (!isset($install_info[$key])) {
+							$install_info[$key] = '';
+						}
+					}
+
 
 					$zip->close();
 				} else {
@@ -537,10 +544,10 @@ class Installer extends \Ventocart\System\Engine\Controller
 		} else {
 			$json['error'] = $this->language->get('error_extension');
 		}
-
 		if (!$json) {
 			$zip = new \ZipArchive();
-
+			$filelist = [];
+			$code = strtolower($extension_install_info['code']);
 			if ($zip->open($file, \ZipArchive::RDONLY)) {
 				$total = $zip->numFiles;
 				$limit = 200;
@@ -548,14 +555,74 @@ class Installer extends \Ventocart\System\Engine\Controller
 				$start = ($page - 1) * $limit;
 				$end = $start > ($total - $limit) ? $total : ($start + $limit);
 
+				// Flag to check if /upload/ folder exists
+				$root_is_upload = false;
+
+				// Iterate through the files to check if there's a /upload/ directory
+				for ($i = 0; $i < $total; $i++) {
+					$source = $zip->getNameIndex($i);
+					$source = str_replace('\\', '/', $source); // Normalize slashes
+
+					// Check if the file path starts with 'upload/'
+					if (strpos($source, 'upload/') === 0) {
+						$root_is_upload = true;
+						break; // No need to check further once we find the upload folder
+					}
+				}
+
+				// Now process the ZIP files based on the existence of the /upload/ root folder
 				for ($i = $start; $i < $end; $i++) {
 					$source = $zip->getNameIndex($i);
+					$source = str_replace('\\', '/', $source); // Normalize slashes
 
-					// Normalize slashes
-					$source = str_replace('\\', '/', $source);
+					// If the root is "upload", remove it from the source path
+					if ($root_is_upload) {
+						// Remove 'upload/' from the start of the path
+						$source = substr($source, 6); // Skip the "upload/" prefix
+					}
 
-					// For theme files, we only want to extract certain directories (themes/code/plates, themes/code/assets, etc.)
+					//check for system library or other 
+					$expl = explode("/", ltrim($source, '/'));
+					$issystem = false;
+					if (($expl[0] == "system" || $expl[0] == "library") && $expl[1] != "extension") {
+						$source = str_replace('system/', "/system/extension/$code/", $source);
+						$issystem = true;
+
+					}
+					if (isset($expl[3]) && ($expl[1] == "language") && $expl[3] != "extension") {
+						$source = str_replace($expl[2], '', $source);
+						$source = str_replace('language/', "language/" . $expl[2] . "/extension/$code", $source);
+
+
+
+					}
+
+					$sourceb = $source;
+					$source = str_replace('admin/view/template', 'admin/view/default/plates/extension/' . $code, $source);
+					if ($sourceb != $source) {
+						$source = str_replace("$code/extension", "$code/", $source);
+					}
+					if (
+						str_replace('admin/view/', '', $source) != $source &&
+						str_replace('admin/view/default/plates', '', $source) == $source
+					) {
+						$source = str_replace('admin/view', 'admin/view/default/assets/extension/' . $code, $source);
+					}
+					$source = str_replace('catalog/view/theme/default/template', 'themes/default/plates/extension/' . $code, $source);
+
+					$source = str_replace('catalog/view/template', 'themes/default/plates/extension/' . $code, $source);
+
+
+					if (
+						str_replace('catalog/view/', '', $source) != $source &&
+						str_replace('catalog/view/default/plates', '', $source) == $source
+					) {
+						$source = str_replace('catalog/view', 'themes/default/assets/extension/' . $code, $source);
+					}
+
+					// If it's not part of the theme directory, continue as usual
 					if ($is_theme) {
+						// For theme files, we only want to extract certain directories (themes/code/plates, themes/code/assets, etc.)
 						if (strpos($source, "themes/") === 0) {
 							// Remove the 'theme/' part and extract into the appropriate theme directory in Ventocart
 							$target_path = DIR_VENTOCART . "/themes/" . substr($source, 6); // Skip the "theme/" prefix
@@ -581,20 +648,51 @@ class Installer extends \Ventocart\System\Engine\Controller
 						}
 					} else {
 						// For extensions, process as usual
-						if ($source === 'install.json' && !$is_theme) {
+
+						if ($source === 'install.json') {
 							continue;
 						}
 
-
 						if (substr(ltrim(trim($source), '/'), 0, 5) == "admin") {
-
 							$source = $admin_path . substr(ltrim(trim($source), '/'), 5);
+						}
+						// Remove any _ from the filenames
+
+						if (preg_match('#/(controller|model|view|language)/#i', $source) && substr($source, -4) === '.php') {
+							// Extract the file name and path
+							$file_info = pathinfo($source);
+
+							// Replace underscores in the filename
+
+							$new_filename = str_replace('_', '', $file_info['filename']);
+
+							// Rebuild the full path with the modified filename
+							$source = $file_info['dirname'] . '/' . $new_filename . '.' . $file_info['extension'];
+
 						}
 
 						// Target path in Ventocart structure
 						$target_path = DIR_VENTOCART . $source;
 					}
 
+
+					// OpenCart 2, no stray files in /controller/type, instead /extension/name/type
+					if (str_replace("/controller/extension", "", $target_path) == $target_path) {
+						$target_path = str_replace("/controller", "/controller/extension/$code/", $target_path);
+					}
+					if (str_replace("/model/extension", "", $target_path) == $target_path) {
+						$target_path = str_replace("/model", "/model/extension/$code/", $target_path);
+					}
+
+					// OpenCart 3 compatibility, no stray files in /extension/type/ instead /extension/name/type/
+					if (str_replace("extension/$code", '', $target_path) == $target_path) {
+						$target_path = str_replace('extension/', "extension/$code/", $target_path);
+					}
+
+
+					if (str_replace("catalog/view", '', $target_path) != $target_path) {
+						continue;
+					}
 					// Create directory if it doesn't exist
 					if (substr($source, -1) == '/') {
 						if (!is_dir($target_path)) {
@@ -610,7 +708,26 @@ class Installer extends \Ventocart\System\Engine\Controller
 									throw new \RuntimeException("Failed to create directory: $targetDir");
 								}
 							}
+
+							if (preg_match('#/(controller|model|view|language)/#i', $target_path) && !$issystem) {
+
+								$target_path = strtolower($target_path);
+								if (strpos($target_path, '/view/') !== false && pathinfo($target_path, PATHINFO_EXTENSION) === 'tpl') {
+									// Replace .tpl with .php
+									$target_path = preg_replace('/\.tpl$/i', '.php', $target_path);
+									$filename = pathinfo($target_path, PATHINFO_FILENAME);
+									$filename = str_replace('_', '', $filename);
+
+									// Replace .tpl with .php and update the filename
+									$target_path = dirname($target_path) . '/' . $filename . '.php';
+								}
+								$target_path = str_replace('//', '/', $target_path);
+								$target_path = str_replace('//', '/', $target_path);
+								$filelist[] = $target_path;
+							}
+
 							file_put_contents($target_path, $contents);
+
 						}
 					}
 
@@ -627,7 +744,12 @@ class Installer extends \Ventocart\System\Engine\Controller
 			}
 		}
 
+
+
 		if (!$json) {
+			if (!$is_theme) {
+				$this->sanitizeExtension($filelist, $code);
+			}
 			$json['text'] = sprintf($this->language->get('text_install'), $start, $end, $total);
 
 			$url = '';
@@ -649,7 +771,134 @@ class Installer extends \Ventocart\System\Engine\Controller
 
 
 
+	private function sanitizeExtension($filelist, $code)
+	{
 
+		// Loop through each file in the file list
+
+		foreach ($filelist as $file) {
+			if (in_array(pathinfo($file, PATHINFO_EXTENSION), ['php', 'twig'])) {
+				if (preg_match('#/(controller|model|view)/#i', $file)) {
+					$content = file_get_contents($file);
+					// Lets start by fixing the template paths
+					$admin_path = rtrim(str_replace(DIR_VENTOCART, '', DIR_APPLICATION), '/');
+					// For styles
+					$pattern1 = '/\.\.\/extension\/(\w+)\/admin\/view\/(.*)/i';
+					$replacement1 = "/" . $admin_path . '/view/default/assets/extension/$1/$2';
+					$content = preg_replace($pattern1, $replacement1, $content);
+
+					// Pattern 2: catalog/view/stylesheet/ to theme/default/assets/extension/$code/
+					$pattern2 = '/\.\.\/extension\/(\w+)\/catalog\/view\/(.*)/i';
+					$replacement2 = 'theme/default/assets/extension/$1/$2';
+					$content = preg_replace($pattern2, $replacement2, $content);
+
+					$content = str_ireplace($code, $code, $content);
+					file_put_contents($file, $content);
+				}
+
+			}
+			// Check if it's a PHP file and it is in "controller/" or "model/"
+			if (
+				pathinfo($file, PATHINFO_EXTENSION) === 'php' &&
+				(strpos($file, 'controller/') !== false || strpos($file, 'model/') !== false)
+			) {
+
+				// Read the file content
+				$content = file_get_contents($file);
+
+				// Rule 1: If it already has "namespace Ventocart", skip it
+				if (strpos($content, 'namespace Ventocart\\') !== false) {
+					continue;  // Skip to the next file
+				}
+
+				// Rule 2: If it has "namespace Opencart", replace it with "namespace Ventocart"
+				if (strpos($content, 'namespace Opencart\\') !== false) {
+					$content = str_replace('namespace Opencart\\', 'namespace Ventocart\\', $content);
+					$content = str_replace('\Opencart', '\Ventocart', $content);
+
+					// handle system
+					$content = str_replace("$code/system/", "", $content);
+
+					$content = str_replace('DIR_EXTENSION', 'DIR_SYSTEM . "extension/' . $code . '/"', $content);
+					file_put_contents($file, $content); // Save the file with the new namespace
+					continue; // Skip to the next file
+				}
+
+				// Rule 3: If no namespaces were found, handle based on controller or model path
+				$classPath = '';
+				$hasSomething = false;
+				if (strpos($file, 'controller/') !== false) {
+					$hasSomething = "Controller";
+				}
+				if (strpos($file, 'model/') !== false) {
+					$hasSomething = "Model";
+				}
+				if ($hasSomething) {
+					// Controller files: Modify namespace path according to the controller structure
+					$admin = rtrim(str_replace(DIR_VENTOCART, "", DIR_APPLICATION), '/');
+					$classPath = str_replace($admin, "Admin", $file);
+					$classPath = str_replace(DIR_VENTOCART, '', dirname($classPath)); // Get directory path without DIR_VENTOCART
+					$classPath = 'Ventocart\\' . str_replace(['_', '/'], ['', '\\'], ucwords($classPath, '_/'));
+					$classPathEnd = explode("\\", $classPath);
+					$classPathEnd = end($classPathEnd);
+					$classPath = str_replace('\\\\', '\\', $classPath);
+
+					// Replace class name and insert the namespace
+					$content = str_replace('class ' . $hasSomething, 'class ', $content); // OC 2
+					$content = str_replace('class Extension' . $classPathEnd, 'class ', $content); // OC 3
+					$content = str_replace('extends ' . $hasSomething, "extends \Ventocart\System\Engine\\" . $hasSomething, $content);
+					$content = str_replace('<?php', "<?php \rnamespace " . $classPath . ";\r\r", $content);
+
+
+					// Replace loads() to /extension/name/type/thing 
+					$patterns = [
+						'/view\([\'"]([^\'"]*extension[^\'"]+)[\'"]\)/',  // Match paths in view() starting with 'extension'
+						'/language\([\'"]([^\'"]*extension[^\'"]+)[\'"]\)/',  // Match paths in language() starting with 'extension'
+						'/model\([\'"]([^\'"]*extension[^\'"]+)[\'"]\)/',  // Match paths in model() starting with 'extension'
+					];
+
+					// Loop through patterns
+					foreach ($patterns as $pattern) {
+						// Find all matches in the content
+						preg_match_all($pattern, $content, $matches);
+
+						if (!empty($matches[1])) {
+							foreach ($matches[1] as $match) {
+								// Use a regex to ensure the path starts with 'extension/'
+								if (preg_match('/^extension\//', $match)) {
+									// Extract the last word from the path  
+									$last_segmentf = basename($match);  // Get the last part of the path, filename
+
+									$last_segment = str_replace('_', '', $last_segmentf);  // Remove underscores
+									$content = str_replace($last_segmentf, strtolower($last_segment), $content);
+									$rest_of_path = dirname($match);
+									$new_path = "extension/$code" . substr($rest_of_path, 9) . "/" . strtolower($last_segment);
+									$content = str_replace($match, $new_path, $content);
+								}
+							}
+						}
+					}
+					$content = preg_replace_callback(
+						'/\bclass\s+(\w+)/', // Match "class " followed by the class name
+						function ($matches) {
+							return 'class ' . ucfirst($matches[1]); // Capitalize the first letter of the class name
+						},
+						$content
+					);
+					$content = str_replace("'extension/", "'extension/$code/", $content);
+					$content = str_replace('$this->model_extension_', '$this->model_extension_' . $code . "_", $content);
+					$content = str_replace('$this->session->data[\'token\']', '$this->session->data[\'user_token\']', $content);
+					$content = str_replace('\'token=', '\'user_token=', $content);
+					$content = str_replace('.tpl', '', $content);
+					$content = str_replace('DIR_EXTENSION', 'DIR_SYSTEM . "extension/' . $code . '/"', $content);
+					$content = str_replace("DIR_SYSTEM", "DIR_SYSTEM . 'extension/$code/'", $content);
+
+					// Save the modified file
+					file_put_contents($file, $content);
+				}
+			}
+		}
+	}
 
 
 
@@ -875,8 +1124,11 @@ class Installer extends \Ventocart\System\Engine\Controller
 				new \RecursiveDirectoryIterator($baseDir, \FilesystemIterator::SKIP_DOTS),
 				\RecursiveIteratorIterator::SELF_FIRST
 			);
-
+			$extension_code = strtolower($extension_code);
 			foreach ($iterator as $file) {
+
+
+
 				if ($file->isDir() && strpos($file->getPathname(), "/extension/{$extension_code}/") !== false) {
 					$path = explode("/extension/{$extension_code}/", $file->getPathname());
 					$target_dirs[] = $path[0] . "/extension/{$extension_code}/";
@@ -891,6 +1143,7 @@ class Installer extends \Ventocart\System\Engine\Controller
 
 			// Delete files and directories recursively
 			foreach ($target_dirs as $dir) {
+
 
 				if (is_dir($dir)) {
 
@@ -928,7 +1181,7 @@ class Installer extends \Ventocart\System\Engine\Controller
 	 */
 	private function deleteDirectory(string $dir): void
 	{
-
+		$dir = rtrim(preg_replace('#/+#', '/', $dir), '/');
 		if (is_dir($dir)) {
 
 			$files = array_diff(scandir($dir), array('.', '..'));
